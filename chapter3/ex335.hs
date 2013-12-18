f81e5869492e62a1308ea9d0f9f9f41f0bbbac0f
Eric Fulmer <EFulmer@gmail.com>
2013-12-17 21:19:09 -0500

    fixed ex335

--- chapter3/ex335.hs
+++ chapter3/ex335.hs
@@ -6,3 +6,7 @@ consonants = "[bcdfghjlkmnpqrstvwxyz]*"
 vowels = ["a+", "e+", "i+", "o+", "u+"]
 
 vowelsRegex = mkRegex $ intercalate consonants vowels
+
+-- 3.3.5 b)
+lexOrderStr = [ a:"*" | a <- ['a'..'z'] ]
+lexOrderRegex = (mkRegex . concat) lexOrderStr
--- chapter3/ex_3_3_5.py
+++ chapter3/ex_3_3_5.py
@@ -2,6 +2,7 @@
 # Write regular definitions for the following languages:
 
 import re 
+import string
 
 # a) All strings of lowercase letters that contain the five vowels in 
 # order.
@@ -13,3 +14,16 @@ consonants = 'bcdfghjlkmnpqrstvwxyz'
 lowercase_re = re.compile('[{c}]*a+[{c}]*e+[{c}]*i+[{c}]*o+[{c}]*u+[{c}]'.format(
                             c=consonants))
 
+# b) All strings of lowercase letters in which the letters are in 
+# ascending lexicographic order.
+
+# In other words: any number of as, followed by any number of bs, 
+# so on and so forth, until we reach z.
+# More formally, the regular def. for this language would be 
+# something like:
+# a_def -> 'a*'
+# b_def -> a_def|'b*'
+# c_def -> b|c*
+# etc.
+# Sigma is {}.
+lex_asc_re = re.compile('a*b*c*d*e*f*g*h*i*j*k*l*m*n*o*p*q*r*s*t*u*v*w*x*y*z*')
